d80bda5608373ae3600acf15bc6db6981fd09f99
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-08-03 15:32:02 +0300

    Minor changes: enabled UP button for ExampleDetailsActivity.

--- app/src/main/java/com/shamanland/fab/example/ExampleDetailsActivity.java
+++ app/src/main/java/com/shamanland/fab/example/ExampleDetailsActivity.java
@@ -1,7 +1,10 @@
 package com.shamanland.fab.example;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ExampleDetailsActivity extends Activity {
     @Override
@@ -9,5 +12,23 @@ public class ExampleDetailsActivity extends Activity {
         super.onCreate(state);
 
         setContentView(R.layout.a_example_details);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar ab = getActionBar();
+            if (ab != null) {
+                ab.setDisplayHomeAsUpEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
